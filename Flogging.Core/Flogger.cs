123e2147537fa2b02cf048c2c6e582a203711dee
Jassar Al_Akhars <902703@mof.gov.sa>
2020-05-29 17:59:52 +0300

    console logger

---
+++ Flogging.Core/Flogger.cs
@@ -0,0 +1,113 @@
+﻿using Serilog;
+using Serilog.Events;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Flogging.Core
+{
+    public static class Flogger
+    {
+        private static readonly ILogger _perfLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+        static Flogger()
+        {
+            CheckFilePath();
+            _perfLogger = new LoggerConfiguration()
+                .WriteTo.File(path: @"D:\Flogging\Flogging\Flogging.Core\loggerFile\perf.txt")
+                .CreateLogger();
+
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.File(path: @"D:\Flogging\Flogging\Flogging.Core\loggerFile\usage.txt")
+                .CreateLogger();
+
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.File(path: @"D:\Flogging\Flogging\Flogging.Core\loggerFile\error.txt")
+                .CreateLogger();
+
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.File(path: @"D:\Flogging\Flogging\Flogging.Core\loggerFile\diagnostic.txt")
+                .CreateLogger();
+        }
+
+        private static void CheckFilePath()
+        {
+            if (!File.Exists(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\perf.txt"))
+            {
+                File.Create(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\perf.txt");
+            }
+            if (!File.Exists(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\usage.txt"))
+            {
+                File.Create(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\usage.txt");
+            }
+            if (!File.Exists(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\error.txt"))
+            {
+                File.Create(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\error.txt");
+            }
+            if (!File.Exists(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\diagnostic.txt"))
+            {
+                File.Create(@"D:\Flogging\Flogging\Flogging.Core\loggerFile\diagnostic.txt");
+            }
+        }
+
+        public static void WritePerf(FlogDetail infoToLog)
+        {
+            _perfLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+        public static void WriteUsage(FlogDetail infoToLog)
+        {
+            _usageLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+        public static void WriteDiagnostic(FlogDetail infoToLog)
+        {
+            var writeDiagnostics = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableDiagnostics"]);
+            if (!writeDiagnostics)
+                return;
+
+            _diagnosticLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+
+
+        public static void WriteError(FlogDetail infoToLog)
+        {
+            if (infoToLog.Exception != null)
+            {
+                var procName = FindProcName(infoToLog.Exception);
+                infoToLog.Location = string.IsNullOrEmpty(procName) ? infoToLog.Location : procName;
+                infoToLog.Message = GetMessageFromException(infoToLog.Exception);
+            }
+            _errorLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+        private static string FindProcName(Exception ex)
+        {
+            var sqlEx = ex as SqlException;
+            if (sqlEx != null)
+            {
+                var procName = sqlEx.Procedure;
+                if (!string.IsNullOrEmpty(procName))
+                    return procName;
+            }
+
+            if (!string.IsNullOrEmpty((string)ex.Data["Procedure"]))
+            {
+                return (string)ex.Data["Procedure"];
+            }
+
+            if (ex.InnerException != null)
+                return FindProcName(ex.InnerException);
+
+            return null;
+        }
+
+        private static string GetMessageFromException(Exception ex)
+        {
+            if (ex.InnerException != null)
+                return GetMessageFromException(ex.InnerException);
+
+            return ex.Message;
+        }
+    }
+}
---
+++ FloggingConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using Flogging.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloggingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fd = GetFlogDetail("starting application", null);
+            Flogger.WriteDiagnostic(fd);
+
+            var tracker = new PerfTracker("FloggerConsole_Execution", "", fd.UserName, fd.Location, fd.Product, fd.Layer);
+            try
+            {
+                var ex = new Exception("Something bad has happened!");
+                ex.Data.Add("input param", "nothing to see here");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                fd = GetFlogDetail("", ex);
+                Flogger.WriteError(fd);
+            }
+            fd = GetFlogDetail("used flogging console", null);
+            Flogger.WriteUsage(fd);
+            fd = GetFlogDetail("stopping the application", null);
+            Flogger.WriteDiagnostic(fd);
+
+            tracker.Stop();
+        }
+        private static FlogDetail GetFlogDetail(string message, Exception ex)
+        {
+            const string Product = "Flogger";
+            const string Location = "FloggerConsole"; // this application
+            const string Layer = "Job"; // unattended executable invoked somehow
+            var user = Environment.UserName;
+            var hostname = Environment.MachineName;
+
+            return new FlogDetail
+            {
+                Product = Product,
+                Location = Location,
+                Layer = Layer,
+                UserName = user,
+                Hostname = hostname,
+                Message = message,
+                Exception = ex
+            };
+        }
+
+    }
+
+}
